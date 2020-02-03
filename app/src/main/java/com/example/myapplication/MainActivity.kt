9d6194fd94068819c9213630082f57ad3c11c68a
K-HUB  LATITUDE <borebrian77@gmai.com>
2020-02-03 04:04:54 -0800

    V2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,50 @@
-package com.example.myapplication
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.DataSource
+import com.example.myapplication.R
+import com.example.myapplication.models.BlogRecyclerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var blogAdapter: BlogRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRecyclerView()
+        addDataSet()
     }
+
+    private fun addDataSet(){
+        val data = DataSource.createDataSet()
+        blogAdapter.submitList(data)
+    }
+
+    private fun initRecyclerView(){
+
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+
+
+            blogAdapter = BlogRecyclerAdapter()
+            adapter = blogAdapter
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
